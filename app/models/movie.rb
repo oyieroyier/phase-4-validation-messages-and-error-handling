2e5592eec529224472bb543a2ae4ce4702a01fda
Bob Oyier <boboyier92@gmail.com>
2023-03-17 14:58:23 +0300

    Done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,25 @@
 class Movie < ApplicationRecord
+  CATEGORIES = %w[
+    Comedy
+    Drama
+    Animation
+    Mystery
+    Fantasy
+    Action
+    Documentary
+    SciFi
+  ].freeze
+
+  validates :title, presence: true
+  validates :year,
+            numericality: {
+              greater_than_or_equal_to: 1888,
+              less_than_or_equal_to: Date.today.year
+            }
+  validates :poster_url, presence: true
+  validates :category,
+            inclusion: {
+              in: CATEGORIES,
+              message: "must be one of: #{CATEGORIES.join(', ')}"
+            }
 end
